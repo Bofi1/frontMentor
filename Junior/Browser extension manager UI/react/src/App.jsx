d840decb83f4bd7d6b83c77ddfb93fa1acd404db
Bofi1 <angelborque03github@gmail.com>
2025-09-19 17:39:17 -0700

    ??

--- Junior/Browser extension manager UI/react/src/App.jsx
+++ Junior/Browser extension manager UI/react/src/App.jsx
@@ -1,11 +1,16 @@
 import Filters from "./components/Filters";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import RenderElements from "./components/RenderElements";
 
 function App() {
   return (
     <>
       <Header />
       <Filters />
+      <Main>
+        <RenderElements />
+      </Main>
     </>
   );
 }
--- Junior/Browser extension manager UI/react/src/components/Main.jsx
+++ Junior/Browser extension manager UI/react/src/components/Main.jsx
@@ -1,9 +1,11 @@
-function Main() {
+function Main(props) {
   return (
     <main
       className="grid grid-cols-1 gap-5 tablet:grid-cols-2 desktop:grid-cols-3"
       id="extensions-list"
-    ></main>
+    >
+      {props.children}
+    </main>
   );
 }
 
