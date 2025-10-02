83b826f7e89c800f39ee5ea553dce725b3d6ef07
Bofi1 <angelborque03github@gmail.com>
2025-10-01 19:47:50 -0700

    desktop

--- Junior/1.testimonial-grid-section/tailwind.config.js
+++ Junior/1.testimonial-grid-section/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         Barlow: ["'Barlow Semi Condensed'", "sans-serif"],
       },
+      screens: {
+        desktop: "1200px",
+      },
     },
   },
   plugins: [],
