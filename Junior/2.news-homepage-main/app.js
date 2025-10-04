1287db0113b6ad2c052e168c3c34315bb59babd9
Bofi1 <angelborque03github@gmail.com>
2025-10-03 22:19:07 -0700

    menu-toggle-mobile

--- Junior/2.news-homepage-main/app.js
+++ Junior/2.news-homepage-main/app.js
@@ -1,13 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-  buttonMenu = document.getElementById("button-menu");
+  let buttonMenu = document.getElementById("button-menu");
+  let body = document.getElementById("body");
 
   buttonMenu.addEventListener("click", () => {
     console.log(buttonMenu.src);
 
     if (buttonMenu.src.includes("/assets/images/icon-menu.svg")) {
       buttonMenu.src = "./assets/images/icon-menu-close.svg";
+      menuToggle();
     } else {
       buttonMenu.src = "./assets/images/icon-menu.svg";
+      menuToggle();
     }
   });
+
+  let menu = document.getElementById("menu");
+  function menuToggle() {
+    menu.classList.toggle("menu-closed");
+    body.classList.toggle("bg-black/50");
+  }
 });
