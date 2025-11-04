aa31871f7e85c698e2ca1d17d5c011f95e167d17
Bofi1 <angelborque03github@gmail.com>
2025-11-03 19:05:12 -0700

    agency landing page

---
+++ Junior/4.agency-landing-page-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Fraunces:["'Fraunces'", "sans-serif"],
+        Barlow:["'Barlow'", "sans-serif"],
+
+      }
+    },
+  },
+  plugins: [],
+}
+
