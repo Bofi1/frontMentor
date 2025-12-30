17df928ec2619684fabda48a8d0cebe4a29782eb
Bofi1 <angelborque03github@gmail.com>
2025-12-30 13:47:42 -0700

    resposive

--- react-junior/proyects/testimonial-slider/src/components/Slider.jsx
+++ react-junior/proyects/testimonial-slider/src/components/Slider.jsx
@@ -34,7 +34,7 @@ function Slider() {
 
   return (
     <div className="w-full flex flex-col items-center lg:flex-row ">
-      <div className="inline-flex flex-col  items-center lg:order-2">
+      <div className="inline-flex flex-col  items-center lg:order-2 relative">
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
             key={secuence}
@@ -44,12 +44,23 @@ function Slider() {
             transition={{ duration: 0.1 }}
           >
             <img
-              className="h-50 lg:h-[500px] rounded-lg shadow-xl"
+              className="h-50 lg:h-[500px] rounded-lg shadow-xl relative z-99"
               src={media[secuence].url}
               alt=""
             />
+            <img
+              class="absolute top-0 scale-135"
+              alt=""
+              src="/img/pattern-bg.svg"
+            ></img>
           </motion.div>
         </AnimatePresence>
+
+        <img
+          class="absolute lg:-left-85 lg:top-21 -bottom-23 scale-45 lg:scale-100"
+          alt=""
+          src="/img/pattern-quotes.svg"
+        ></img>
 
         <div className=" shadow-lg rounded-full inline -mt-3 z-99 lg:mr-[300px] lg:-mt-6">
           <button
@@ -77,21 +88,36 @@ function Slider() {
       <AnimatePresence mode="wait" initial={false}>
         <motion.div
           key={secuence}
-          initial={{ opacity: 0, x: -10 }}
+          initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -10 }}
+          exit={{ opacity: 0, x: -50 }}
           transition={{ duration: 0.1 }}
-          className="text-center max-w-80 mt-10 lg:order-1 flex flex-col lg:max-w-[490px] lg:-mr-[60px] z-98"
+          className="text-center max-w-80 mt-10 lg:order-1 flex flex-col lg:max-w-[490px] lg:-mr-[60px] relative z-[100]"
         >
-          <p className="text-start">{media[secuence].description}</p>
-          <div className="text-[16px] mt-5 lg:flex gap-2">
+          <p className="lg:text-start text-[18px] lg:text-[25px]">
+            {media[secuence].description}
+          </p>
+          <motion.div
+            key={secuence}
+            initial={{ opacity: 0, x: 70 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 70 }}
+            transition={{ duration: 0.1 }}
+            className="text-[12px] lg:text-[16px] mt-5 lg:flex gap-2"
+          >
             <p className="font-bold ">{media[secuence].name}</p>
             <p className="font-[600] text-[#CBCAD3]">
               {media[secuence].occupation}
             </p>
-          </div>
+          </motion.div>
         </motion.div>
       </AnimatePresence>
+
+      <img
+        class="absolute bottom-0 left-0 "
+        alt=""
+        src="/img/pattern-curve.svg"
+      ></img>
     </div>
   );
 }
