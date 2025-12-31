605739014f90aa9b5d7bef31a49c4371da7b8829
Bofi1 <angelborque03github@gmail.com>
2025-12-30 22:01:03 -0700

    imports

---
+++ react-junior/proyects/mortgage-calculator/src/components/MortgageCalculator.jsx
@@ -0,0 +1,12 @@
+import MortgageForm from "./MortgageForm";
+import MortgageResults from "./MortgageResults";
+
+function MortgageCalculator() {
+  return (
+    <div>
+      <div>MortgageCalculator</div>
+    </div>
+  );
+}
+
+export default MortgageCalculator;
---
+++ react-junior/proyects/mortgage-calculator/src/components/MortgageForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MortgageForm() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default MortgageForm;
