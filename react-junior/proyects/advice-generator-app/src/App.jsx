92a51100a2271b99fc837c4ef53524c9e31fc967
Bofi1 <angelborque03github@gmail.com>
2026-02-26 20:38:21 -0700

    1st commit

---
+++ react-junior/proyects/advice-generator-app/src/App.jsx
@@ -0,0 +1,7 @@
+import AdviceGenApp from "./components/AdviceGenApp";
+
+function App() {
+  return <AdviceGenApp />;
+}
+
+export default App;
---
+++ react-junior/proyects/advice-generator-app/src/components/AdviceGenApp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AdviceGenApp() {
+  return (
+    <div className="bg-[#303A49] font-Manrope rounded-xl shadow-lg px-5 py-10 text-center">
+      <span className="text-[#5EE3A4] text-[0.8rem] font-[800] tracking-[0.2rem] ">
+        ADVICE #117
+      </span>
+      <p className="text-[#CCDFE8] font-bold text-2xl leading-9 mt-5">
+        "It is easy to sit up and take notice, what´s difficult is getting up
+        and taking action."
+      </p>
+    </div>
+  );
+}
+
+export default AdviceGenApp;
